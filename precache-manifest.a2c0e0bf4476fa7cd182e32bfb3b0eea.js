cdac29547be57169d55de75137aff1a542dba111
Nikhil Singh <nikhilbangalor@gmail.com>
2020-08-09 23:47:28 +0530

    Updates

---
+++ precache-manifest.a2c0e0bf4476fa7cd182e32bfb3b0eea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4274c3cc7d894442861bef48943135",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "e87ff1f58eb4eb5b06ca",
+    "url": "/Covid-Tracker/static/css/main.42df3774.chunk.css"
+  },
+  {
+    "revision": "2c4d3e2fc9960e8f725a",
+    "url": "/Covid-Tracker/static/js/2.289e0764.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-Tracker/static/js/2.289e0764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87ff1f58eb4eb5b06ca",
+    "url": "/Covid-Tracker/static/js/main.04db436a.chunk.js"
+  },
+  {
+    "revision": "b619aef232c750e36b8d",
+    "url": "/Covid-Tracker/static/js/runtime-main.fe252f1e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Tracker/static/media/image.d7265326.png"
+  }
+]);
